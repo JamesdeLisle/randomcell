2e9a8cd96f60ebbdab1d4d9b955ab36244c251b4
JamesdeLisle <lpko908@gmail.com>
2015-08-23 22:00:11 +0100

    none

--- utility_functions.py
+++ utility_functions.py
@@ -1,7 +1,7 @@
 import pygame
 import numpy as np
 from numpy.random import random_sample
-from levelgen import *
+from numpy.random import random_integers as rand
 
 def discreteDraw(values,probabilities):
 
@@ -45,6 +45,32 @@ def createWallMap(width,height):
                     wallMap[tik1][tik2] = True
 
     return wallMap
+
+def generate_maze(width, height, complexity=.01, density=.05):
+    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
+    complexity = int(complexity * (5 * (shape[0] + shape[1])))
+    density    = int(density * (shape[0] // 2 * shape[1] // 2))
+    Z = np.zeros(shape, dtype=bool)
+    Z[0, :] = Z[-1, :] = 1
+    Z[:, 0] = Z[:, -1] = 1
+
+    for i in range(density):
+        x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2
+        Z[y, x] = 1
+        for j in range(complexity):
+            neighbours = []
+            if x > 1:             neighbours.append((y, x - 2))
+            if x < shape[1] - 2:  neighbours.append((y, x + 2))
+            if y > 1:             neighbours.append((y - 2, x))
+            if y < shape[0] - 2:  neighbours.append((y + 2, x))
+            if len(neighbours):
+                y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
+                if Z[y_, x_] == 0:
+                    Z[y_, x_] = 1
+                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                    x, y = x_, y_
+
+    return Z.tolist()
 
 def getAdjacent(wallMap,x,y):
 
